0b2dce4fe521b4762b424e0673807a6a22a69977
Cugurel <cugurel7@gmail.com>
2024-11-10 12:40:08 +0300

    Add project files.

---
+++ Variable/Program.cs
@@ -0,0 +1,15 @@
+﻿//integer veri tipi -2.147.483.648 ile bunun
+//pozitif değeri arasında değer alır
+//byte değişkeni 0-255 arasında değer alıyormuş
+//sbyte değişkeni -128 - 127 arasında değer alıyormuş
+int a = 5;
+char letter = 'a';
+string name = "Çağrı Uğurel";
+byte number = 100;
+sbyte numbersByte = -100;
+
+Console.WriteLine(a);
+Console.WriteLine(letter);
+Console.WriteLine(name);
+Console.WriteLine(number);
+Console.WriteLine(numbersByte);
