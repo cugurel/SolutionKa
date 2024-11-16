76b564a4406922ea43a706fe1c2d15eb2c014a06
Cugurel <cugurel7@gmail.com>
2024-11-16 12:12:30 +0300

    sayı tek mi çift mi

--- Variable/Program.cs
+++ Variable/Program.cs
@@ -10,18 +10,46 @@
 //bool isOkey = true;
 //float fNumber = 64.231321f;
 
-string name=String.Empty;
-string surname = String.Empty;
-int age;
-Console.WriteLine("Lütfen isminizi giriniz: ");
-name = Console.ReadLine();
-Console.WriteLine("İsminiz: " + name);
+
+//Kullanıcıdan giriş alma
+//string name=String.Empty;
+//string surname = String.Empty;
+//int age;
+//Console.WriteLine("Lütfen isminizi giriniz: ");
+//name = Console.ReadLine();
+//Console.WriteLine("İsminiz: " + name);
 
 
-Console.WriteLine("Lütfen soy isminizi giriniz: ");
-surname = Console.ReadLine();
-Console.WriteLine("Soy isminiz: " + surname);
+//Console.WriteLine("Lütfen soy isminizi giriniz: ");
+//surname = Console.ReadLine();
+//Console.WriteLine("Soy isminiz: " + surname);
 
-Console.WriteLine("Yaşınızı giriniz: ");
-age = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Yaşınız: " + age);
+//Console.WriteLine("Yaşınızı giriniz: ");
+//age = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Yaşınız: " + age);
+
+//Kullanıcının girdiği iki sayının ortalamasını alma
+//int a, b, sum, avg;
+//Console.WriteLine("İlk sayıyı giriniz: ");
+//a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("İkinci sayıyı giriniz: ");
+//b = Convert.ToInt32(Console.ReadLine());
+//sum = a + b;
+//avg = sum / 2;
+//Console.WriteLine("İki sayının ortalaması : " + avg);
+
+//Girilen sayının tek mi çift mi karar veren yazılım
+int number;
+Console.WriteLine("Sayı giriniz: ");
+number =  Convert.ToInt32(Console.ReadLine());
+
+int mod = number % 2;
+
+if(mod == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir");
+}
