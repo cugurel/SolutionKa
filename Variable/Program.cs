82457029a11e99b5a1ffd580ccad1f25307b3b23
Cugurel <cugurel7@gmail.com>
2024-11-16 11:09:43 +0300

    Ekrandan bilgi alma

--- Variable/Program.cs
+++ Variable/Program.cs
@@ -2,14 +2,26 @@
 //pozitif değeri arasında değer alır
 //byte değişkeni 0-255 arasında değer alıyormuş
 //sbyte değişkeni -128 - 127 arasında değer alıyormuş
-int a = 5;
-char letter = 'a';
-string name = "Çağrı Uğurel";
-byte number = 100;
-sbyte numbersByte = -100;
+//int a = 5;
+//char letter = 'a';
+//string name = "Çağrı Uğurel";
+//byte number = 100;
+//sbyte numbersByte = -100;
+//bool isOkey = true;
+//float fNumber = 64.231321f;
 
-Console.WriteLine(a);
-Console.WriteLine(letter);
-Console.WriteLine(name);
-Console.WriteLine(number);
-Console.WriteLine(numbersByte);
+string name=String.Empty;
+string surname = String.Empty;
+int age;
+Console.WriteLine("Lütfen isminizi giriniz: ");
+name = Console.ReadLine();
+Console.WriteLine("İsminiz: " + name);
+
+
+Console.WriteLine("Lütfen soy isminizi giriniz: ");
+surname = Console.ReadLine();
+Console.WriteLine("Soy isminiz: " + surname);
+
+Console.WriteLine("Yaşınızı giriniz: ");
+age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Yaşınız: " + age);
