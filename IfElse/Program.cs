9d601fb3605574904fe7ed66f6d7a36580f2bc69
Cugurel <cugurel7@gmail.com>
2024-11-17 11:04:02 +0300

    if-else example

---
+++ IfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿//Girilen sayının pozitif, nagatif ve ya sıfıra eşit ise yazdırır
+
+//int a;
+//Console.WriteLine("Bir sayı giriniz");
+//a = Convert.ToInt32(Console.ReadLine());
+
+//if(a < 0)
+//{
+//    Console.WriteLine("Sayı negatif");
+//}else if(a == 0)
+//{
+//    Console.WriteLine("Sayı 0'a eşit");
+//}
+//else
+//{
+//    Console.WriteLine("Sayı pozitif");
+//}
+
+
