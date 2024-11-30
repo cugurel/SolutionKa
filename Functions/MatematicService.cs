f05679cd7fcb2cf85b8a8ea621eaf404f8fd6511
Cugurel <cugurel7@gmail.com>
2024-11-30 11:50:59 +0300

    interface ve class kullanımı

---
+++ Functions/MatematicService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public interface MatematicService
+    {
+        int Add(int a, int b);
+        int Multiply(int a, int b);
+        int Division(int a, int b);
+        int Substraction(int a, int b);
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿//string name = "Çağrı";
+//string surname = "Uğurel";
+//void WriteNameAndSurname(string name, string surname)
+//{
+//    Console.WriteLine(name+" "+surname);
+//}
+
+
+//WriteNameAndSurname(name,surname);
+
+int a = 5;
+int b = 7;
+
+int toplam;
+
+int Topla(int sayi1, int sayi2)
+{
+    return sayi1 + sayi2;
+}
+
+toplam = Topla(a, b);
+Console.WriteLine(toplam);
